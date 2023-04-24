165a18d00896a839639075d326051d204dab2beb
Stephan Mandl <mandl737@gmail.com>
2023-04-24 17:33:43 +0200

    replaced path to web driver with relative path

--- java-crawler/src/main/java/configuration/Configuration.java
+++ java-crawler/src/main/java/configuration/Configuration.java
@@ -1,9 +1,11 @@
 package configuration;
 
+import java.nio.file.Path;
+
 public class Configuration {
 
     public final static boolean WEBDRIVER_IS_HEADLESS =false;
-    public final static String WEBDRIVER_URL="C:\\Users\\mandl\\IdeaProjects\\Playground\\chromedriver_win32\\chromedriver.exe";
+    public final static String WEBDRIVER_URL= ("chromedriver_win32\\chromedriver.exe");
 
     public final static int NUMBER_OF_WEBSITES=0;
 
