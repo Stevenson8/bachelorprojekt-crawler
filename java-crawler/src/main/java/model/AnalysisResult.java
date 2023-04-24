dadc093eedab35257aa7fefa3f544a15a96b4805
Stephan Mandl <mandl737@gmail.com>
2023-04-24 19:17:21 +0200

    implemented analysis result class

--- java-crawler/src/main/java/model/AnalysisResult.java
+++ java-crawler/src/main/java/model/AnalysisResult.java
@@ -5,4 +5,12 @@ import java.util.Map;
 public class AnalysisResult {
 
     private Map<Website,WebsiteResult> analysisResult;
+
+    public void addWebsiteResult(Website website, WebsiteResult result){
+        analysisResult.put(website,result);
+    }
+
+    public Map<Website,WebsiteResult> getAnalysisResult(){
+        return analysisResult;
+    }
 }
